5190785b9778eedfde5bf58341de0ddb775f26bd
Dmitry Kabardinov <Dmitry_Kabardinov@epam.com>
2015-12-02 10:44:43 +0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name untitled1App
+ * @description
+ * # untitled1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('untitled1App', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name untitled1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the untitled1App
+ */
+angular.module('untitled1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
